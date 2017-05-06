7f26091646ab732ac1a15270e036d30ed49c899d
Eric Budai <eric_budai@hotmail.com>
2017-05-06 11:53:09 -0400

    Add files via upload

---
+++ TOJam2017/Assets/PilotController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PilotController : MonoBehaviour {
+
+    //private float Thrust { get; set; }
+    private float MaxThrust { get; set; }
+
+    private Rigidbody cube;
+    
+    // Use this for initialization
+    void Start () {
+        MaxThrust = 100;
+        cube = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleRotation();
+        HandleThrusting();
+	}
+    
+    private void HandleThrusting()
+    {
+        var thrust = 40 * Input.GetAxis("Thrust");
+        thrust = Math.Min(thrust, MaxThrust);
+        thrust = Math.Max(thrust, 0);
+        cube.AddForce(transform.forward * thrust);
+    }
+
+    private void HandleRotation()
+    {
+        var rotationAmount = Input.GetAxis("Rotation");
+        var verticalAmount = Input.GetAxis("Vertical");
+        var horizontalAmount = Input.GetAxis("Horizontal");
+
+        cube.AddTorque(-verticalAmount, horizontalAmount, 0);
+        cube.AddRelativeTorque(0, 0, rotationAmount);
+    }
+}
