f87a0231d4159f208f98a99a28167e2b3556049c
ebudai <eric_budai@hotmail.com>
2017-05-06 14:25:10 -0400

    lasers shoot and stuff

--- TOJam2017/Assets/PilotController.cs
+++ TOJam2017/Assets/PilotController.cs
@@ -8,26 +8,35 @@ public class PilotController : MonoBehaviour {
     //private float Thrust { get; set; }
     private float MaxThrust { get; set; }
 
-    private Rigidbody cube;
-    
+    private Rigidbody ship;
+    private ParticleSystem cannon;
+
     // Use this for initialization
     void Start () {
         MaxThrust = 100;
-        cube = GetComponent<Rigidbody>();
+        ship = GetComponent<Rigidbody>();
+        cannon = GameObject.Find("Player Lasers").GetComponent<ParticleSystem>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         HandleRotation();
         HandleThrusting();
+        HandleShooting();
 	}
     
+    private void HandleShooting()
+    {
+        if (Input.GetAxis("Fire") != 0) cannon.Play();
+        else cannon.Stop();
+    }
+
     private void HandleThrusting()
     {
         var thrust = 40 * Input.GetAxis("Thrust");
         thrust = Math.Min(thrust, MaxThrust);
         thrust = Math.Max(thrust, 0);
-        cube.AddForce(transform.forward * thrust);
+        ship.AddForce(transform.forward * thrust);
     }
 
     private void HandleRotation()
@@ -36,7 +45,7 @@ public class PilotController : MonoBehaviour {
         var verticalAmount = Input.GetAxis("Vertical");
         var rotationAmount = Input.GetAxis("Horizontal");
 
-        cube.AddTorque(0, -horizontalAmount, 0);
-        cube.AddRelativeTorque(-verticalAmount, 0, -rotationAmount);
+        ship.AddTorque(0, -horizontalAmount, 0);
+        ship.AddRelativeTorque(-verticalAmount, 0, -rotationAmount);
     }
 }
